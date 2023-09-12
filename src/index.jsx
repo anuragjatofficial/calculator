87a7cd3fc1e212024634b7acc42089d84df4d805
anuragjatofficial <anurag.1101140@gmail.com>
2023-09-13 02:29:25 +0530

    added styles

--- src/index.jsx
+++ src/index.jsx
@@ -75,126 +75,126 @@ function Calculator(){
 
     return (
       <div className="flex justify-center flex-col items-center h-screen bg-black">
-        <div id="calculator" className="grid grid-cols-4 gap-2">
+        <div id="calculator" className="grid grid-cols-4 gap-2 border-2 px-2 py-2">
           <div id="display" className="bg-white text-center py-3 px-3">
             {value}
           </div>
           {/* buttons here */}
           <div
             id="clear"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={handleReset}
           >
             AC
           </div>
           <div
             id="divide"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             /
           </div>
           <div
             id="multiply"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             X
           </div>
           <div
             id="seven"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             7
           </div>
           <div
             id="eight"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             8
           </div>
           <div
             id="nine"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             9
           </div>
           <div
             id="subtract"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             -
           </div>
           <div
             id="four"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             4
           </div>
           <div
             id="five"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             5
           </div>
           <div
             id="six"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             6
           </div>
           <div
             id="add"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             +
           </div>
           <div
             id="one"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             1
           </div>
           <div
             id="two"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             2
           </div>
           <div
             id="three"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             3
           </div>
           <div
             id="equals"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={calculate}
           >
             =
           </div>
           <div
             id="zero"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             0
           </div>
           <div
             id="decimal"
-            className="bg-gray-700 text-center py-3 px-3 click"
+            className="bg-gray-700 text-center py-3 px-3 click cursor-pointer hover:text-white hover:border-white hover:border-2"
             onClick={(e) => handleInput(e)}
           >
             .
